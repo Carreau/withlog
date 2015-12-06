edcc2c75e1c9f49a06bf24c9164933c5c9788c1d
Matthias Bussonnier <bussonniermatthias@gmail.com>
2015-12-06 19:51:05 +0100

    add output input context manager and a lot of things

--- withlog/__init__.py
+++ withlog/__init__.py
@@ -1,6 +1,5 @@
-
 """
-a simple package
+
 """
 
 
@@ -11,9 +10,11 @@ from colorama import Fore, Back, Style
 
 colors = True
 
-def bg(message, level=0):
+def bg(message, level=0, fmt=None):
     if colors:
-        return Fore.WHITE+getattr(Back, l[level]) + message + Style.RESET_ALL
+        if not fmt:
+            fmt=Fore.WHITE+getattr(Back, l[level])
+        return fmt + message + Style.RESET_ALL
     else:
         return nc[level]+message
 def fg(message, level=0):
@@ -34,40 +35,80 @@ nc = {
         2: '\u2503',
         3: '\u2503',
 }
-    
 
 
-def glog(message):
+from contextlib import contextmanager
+import builtins
+
+@contextmanager
+def print_statement(p):
+    """replace built-in print statement"""
+    oldprint = builtins.print
+    builtins.print = p 
+    try:
+        yield (p, oldprint)
+    except Exception as e:
+        builtins.print = oldprint
+        print(e)
+
+@contextmanager
+def input():
+    old_input = builtins.input 
+    builtins.input = old_input
+    def new_input(prompt):
+        if gStack:
+            return old_input(gStack[-1].format_line(prompt))
+        else:
+            return old_input(prompt)
+    builtins.input = new_input
+    try:
+        yield 
+    except Exception as e:
+        builtins.input = old_input
+        raise
+    builtins.input = old_input
+
+
+
+def glog(message, *args, **kwargs):
     if gStack:
-        gStack[-1].log('global message')
+        gStack[-1].log(message % tuple( str(a) for a in args))
     else:
         print('>>', message)
 
 class Message:
 
-    def __init__(self, title, level=0):
+    def __init__(self, title, level=0, fmt=None, writer=print):
         self.title = title
         self.level = level
+        self.fmt=fmt
+        self.writer = writer
 
-    def log(self, message=''):
-        return self._log(message)
+    def log(self, message='', *args, **kwargs):
+        return self._log(message, *args, **kwargs)
 
-    def _log(self, message=''):
-        m = '\n'.join([bg(' ', self.level) + ' ' +  m for m in message.split('\n')])
+    def format_line(self, message, *args, **kwargs):
+        if not message: 
+            message = ''
+        message = bg(' ', self.level, self.fmt) + ' ' +  message
         if self.sup:
-            self.sup.log(m)
-        else:
-            print(m)
+            return self.sup.format_line(message)
+        else :
+            return message
+
+    def _log(self, message='', *args, **kwargs):
+        m = '\n'.join([self.format_line(m) for m in message.split('\n')])
+        self.writer(m, *args, **kwargs)
 
     def __enter__(self):
-        if gStack:
+        if gStack and gStack[-1] is not self:
             self.sup = gStack[-1]
-            self.sup.log(' ')
         else:
             self.sup=None
-        gStack.append(self)
+        if self not in gStack:
+            gStack.append(self)
         l = '%s' % (80 - len(gStack)*2)
-        self.log(bg(('{:^'+l+'}').format(self.title), self.level))
+        self.log(bg(('{:^'+l+'}').format(self.title), self.level, self.fmt))
         self.log()
         return self.log
 
@@ -76,12 +117,18 @@ class Message:
         if self.sup:
             self.sup.log(' ')
             self.sup=None
-        gStack.pop()
+        if gStack[-1] is self:
+            gStack.pop()
         pass
+
+class Info(Message):
+    def __init__(self, title):
+        super().__init__(title, 0)
 
 class OK(Message):
     def __init__(self, title):
         super().__init__(title,1)
+
     
 class Warning(Message):
     def __init__(self, title):
